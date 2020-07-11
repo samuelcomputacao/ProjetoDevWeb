62887b3f3a340addd7dff6425c2a290fc7cb1f46
Samuel Vasconcelos <samuel.vasconcelos@ccc.ufcg.edu.br>
2020-07-11 09:46:46 -0300

    crud pedido

--- backend/src/controllers/pedidoController.js
+++ backend/src/controllers/pedidoController.js
@@ -29,7 +29,7 @@ module.exports = {
                 });
                 const idPedido = pedido[0];
                 const pedido_hortalica = await connection('pedido_hortalica').where('key_pedido', '=', key).select('*');
-                pedido_hortalica.forEach(async (ped_hort)=>{
+                pedido_hortalica.forEach(async (ped_hort) => {
                     await connection('pedido_hortalica').insert({
                         key_pedido: idPedido,
                         key_hortalica: ped_hort.key_hortalica,
@@ -37,22 +37,40 @@ module.exports = {
                     });
                 });
                 return response.status(200).send();
-            }else{
-                return response.status(500).json({mensagem:'Apenas o cliente do pedido pode renovar.'});
+            } else {
+                return response.status(500).json({ mensagem: 'Apenas o cliente do pedido pode renovar.' });
             }
 
-        }else{
-            return response.status(500).json({mensagem:'Pedido não encontrado.'});
+        } else {
+            return response.status(500).json({ mensagem: 'Pedido não encontrado.' });
         }
     },
 
     async findById(request, response) {
         const { key } = request.params;
-        const usuario = await connection('usuario').where('key', '=', key).select('*').first();
-        if (usuario) {
-            return response.status(200).json(usuario);
+        const pedido = await connection('pedido').where('key', '=', key).select('*').first();
+        const cliente = await connection('usuario').where('key', '=', pedido.key_cliente).select('*').first();
+        const pedido_hortalica = await connection('pedido_hortalica').where('key_pedido', '=', key).select('*');
+        let hortalicas = []
+        const tam = pedido_hortalica.length;
+        let i = 0;
+        while (i < tam) {
+            const ped_hort = pedido_hortalica[i];
+            const quantidade = ped_hort.quantidade;
+            const hortalica = await connection('hortalica').where('key', '=', ped_hort.key_hortalica).select('*').first();
+            hortalicas = [...hortalicas, { ...hortalica, quantidade }];
+            i++;
         }
-        return response.status(500).json({ mensagem: 'O Usuário não existe.' });
+        if (pedido) {
+            return response.status(200).json({
+                pedido: {
+                    ...pedido,
+                    cliente,
+                    hortalicas
+                }
+            });
+        }
+        return response.status(500).json({ mensagem: 'O pedido não existe.' });
     },
 
     async create(request, response) {
@@ -87,18 +105,35 @@ module.exports = {
 
     async update(request, response) {
         const { key } = request.params;
-        const { nome, funcao, senha } = request.body;
-
-        if (nome) {
-            await connection('usuario').where('key', '=', key).update('nome', nome);
+        const { keyUsuario } = request.query;
+        const { hortalicas } = request.body;
+        const pedido = await connection('pedido').where('key', '=', key).select('*').first();
+        if (pedido) {
+            if (pedido.status === 'REALIZADO') {
+                if (keyUsuario == pedido.key_cliente) {
+                    const pedHort = await connection('pedido_hortalica').where('key_pedido','=',key).select('*');
+                    await connection('pedido_hortalica').where('key_pedido','=',key).delete();
+                    let i = 0;
+                    let hortalica;
+                    while(i < hortalicas.length){
+                        hortalica = hortalicas[i];
+                        await connection('pedido_hortalica').insert({
+                            key_pedido: key,
+                            key_hortalica: hortalica.keyHortalica,
+                            quantidade: hortalica.quantidade
+                        });
+                        i++;
+                    }
+                    return response.status(200).send();
+                } else {
+                    return response.status(401).json({ mensagem: 'O pedido não está associado ao usuário logado' });
+                }
+            } else {
+                return response.status(500).json({ mensagem: 'O pedido não pode ser atualizado.' });
+            }
+        }else{
+            return response.status(500).json({mensagem:'O pedido não existe.'});
         }
-        if (senha) {
-            await connection('usuario').where('key', '=', key).update('senha', senha);
-        }
-        if (funcao) {
-            await connection('usuario').where('key', '=', key).update('funcao', funcao);
-        }
-        return response.json();
     },
 
     async delete(request, response) {
